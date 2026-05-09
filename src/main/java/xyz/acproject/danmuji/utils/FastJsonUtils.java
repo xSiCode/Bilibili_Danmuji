d7f010d4258ce9d2393ad99c116c43667fa9d3e6
xsicode <xsicode@qq.com>
2026-05-09 15:27:24 +0800

    fix backup

--- src/main/java/xyz/acproject/danmuji/utils/FastJsonUtils.java
+++ src/main/java/xyz/acproject/danmuji/utils/FastJsonUtils.java
@@ -6,6 +6,8 @@ import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.serializer.SerializeConfig;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.alibaba.fastjson.serializer.SimpleDateFormatSerializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.Date;
 import java.util.List;
@@ -16,6 +18,8 @@ import java.util.List;
  * @version fastjsonTools v1.0
  */
 public class FastJsonUtils {
+	private static final Logger LOGGER = LogManager.getLogger(FastJsonUtils.class);
+
 	private static final SerializeConfig CONFIG = new SerializeConfig();
 
 	private static final String FORMAT_TIME = "yyyy-MM-dd HH:mm:ss";
@@ -25,75 +29,74 @@ public class FastJsonUtils {
 
 	private static final SerializerFeature[] FEATURES = {
 
-				SerializerFeature.WriteMapNullValue,
+					SerializerFeature.WriteMapNullValue,
 
-				SerializerFeature.WriteDateUseDateFormat,
+					SerializerFeature.WriteDateUseDateFormat,
 
-				SerializerFeature.WriteNullListAsEmpty
+					SerializerFeature.WriteNullListAsEmpty
 
-	}; 
+	};
 
-	
 
-//	WriteMapNullValue
 
-//	WriteDateUseDateFormat
+	//	WriteMapNullValue
 
-	static {
+	//	WriteDateUseDateFormat
 
-		CONFIG.put(Date.class, new SimpleDateFormatSerializer(FORMAT_TIME));
+		static {
 
-	}
-
-	
-
-	public static <T> T parseObject(String json, Class<T> clazz) {
-
-		try {
-
-			T t = JSON.parseObject(json, clazz);
-
-			return t;
-
-		} catch (Exception e) {
-
-			e.printStackTrace();
+			CONFIG.put(Date.class, new SimpleDateFormatSerializer(FORMAT_TIME));
 
 		}
 
-		return null;
-
-	}
 
 
+		public static <T> T parseObject(String json, Class<T> clazz) {
 
-	/**
+			try {
 
-	 * 某人转为yyyy-MM-dd HH:mm:ss格式
+				T t = JSON.parseObject(json, clazz);
 
-	 * @param object
+				return t;
 
-	 * @return
+			} catch (Exception e) {
 
-	 */
+				LOGGER.error("FastJsonUtils parseObject error, json={}", json, e);
 
-	public static String toJson(Object object) {
+			}
 
-		try {
-
-			String json = JSON.toJSONString(object, CONFIG, FEATURES);
-
-			return json;
-
-		} catch (Exception e) {
-
-			e.printStackTrace();
+			return null;
 
 		}
 
-		return null;
 
-	}
+
+		/**
+
+		 * 某人转为yyyy-MM-dd HH:mm:ss格式
+
+		 * @param object
+
+		 * @return
+
+		 */
+
+		public static String toJson(Object object) {
+
+			try {
+
+				String json = JSON.toJSONString(object, CONFIG, FEATURES);
+
+				return json;
+
+			} catch (Exception e) {
+
+				LOGGER.error("FastJsonUtils toJson error, object={}", object, e);
+
+			}
+
+			return null;
+
+		}
 
 }
-
